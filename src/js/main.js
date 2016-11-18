863cc5957ca85815230aa0b00a67010a031d344c
ryan liu <braveoyster@gmail.com>
2016-11-18 17:20:30 +0800

    add feat matches testing

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@
 
 import util from './util.js';
 import Regexper from './regexper.js';
+import TestingMatch from './testing.js';
 import Parser from './parser/javascript.js';
 import _ from 'lodash';
 
@@ -36,6 +37,9 @@ import _ from 'lodash';
     util.tick().then(() => {
       window.dispatchEvent(util.customEvent('hashchange'));
     });
+
+    const testingMatch = new TestingMatch(document.body);
+    testingMatch.bindListeners();
   }
 
   // Initialize other pages on the site (specifically the documentation page).
---
+++ src/js/testing.js
@@ -0,0 +1,76 @@
+import util from './util.js';
+
+export default class MatchTesting {
+  constructor(root) {
+    this.root = root;
+    this.regField = root.querySelector('#regexp-input');
+    this.form = root.querySelector('#testing-form');
+    this.testStringElm = root.querySelector('#test-input');
+    this.globalElm = root.querySelector('#chkGlobalMatch');
+    this.ignoreCaseElm = root.querySelector('#chkIgnoreCase');
+
+    this.testResult = root.querySelector('#test-result');
+    this.errorElm = root.querySelector('#test-error');
+  }
+
+  // Shift-Enter test matches
+  keypressListener(event) {
+    if (event.shiftKey && event.keyCode === 13) {
+      event.returnValue = false;
+      if (event.preventDefault) {
+        event.preventDefault();
+      }
+
+      this.form.dispatchEvent(util.customEvent('submit'));
+    }
+  }
+
+  // Event handler for submission of the testing.
+  submitListener(event) {
+    event.returnValue = false;
+    if (event.preventDefault) {
+      event.preventDefault();
+    }
+
+    try {
+      this.errorElm.style.display = 'none';
+      const regString = this.regField.value;
+      const testString = this.testStringElm.value;
+
+      const options = [];
+      this.globalElm.checked ? options.push('g') : void 0;
+      this.ignoreCaseElm.checked ? options.push('i') : void 0;
+
+      const reg = new RegExp(regString, options.join(''));
+      const result = testString.match(reg);
+
+      if (!result) {
+        this.errorElm.textContent = 'No matches.';
+        this.errorElm.style.display = 'block';
+        this.testResult.style.display = 'none';
+        return;
+      }
+      const frag = document.createDocumentFragment();
+      const lis = result.forEach((mt, idx) => {
+        let li = document.createElement("li");
+        li.textContent = `${idx}: ${mt}`;
+        frag.appendChild(li);
+      });
+      while(this.testResult.firstChild) {
+        this.testResult.removeChild(this.testResult.firstChild);
+      }
+      this.testResult.appendChild(frag);
+    }
+    catch(e) {
+      this.testResult.style.display = 'none';
+      this.errorElm.textContent = e;
+      this.errorElm.style.display = 'block';
+    }
+  }
+
+  // Binds all event listeners.
+  bindListeners() {
+    this.testStringElm.addEventListener('keypress', this.keypressListener.bind(this));
+    this.form.addEventListener('submit', this.submitListener.bind(this));
+  }
+}
